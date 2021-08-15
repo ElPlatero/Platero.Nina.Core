3782375fcc3b238870e56bbcb26cb790a774c321
ElPlatero <elplatero@baelgun.de>
2021-08-15 14:21:39 +0200

    Schnittstelle definiert

---
+++ src/Platero.Nina.Core/Abstractions/IKatWarnMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Platero.Nina.Core.Models;
+
+namespace Platero.Nina.Core.Abstractions
+{
+    /// <summary>
+    /// Verwaltet den Zugriff auf die aktuellen Katastrophen-Warnmeldungen des Bundesamts für Bevölkerungsschutz.
+    /// </summary>
+    public interface IKatWarnMessageRepository
+    {
+        /// <summary>
+        /// Ruft die aktuellen Katastrophen-Warnmeldungen des Bundesamts für Bevölkerungsschutz ab. 
+        /// </summary>
+        /// <returns>Eine Menge von Katastrophen-Warnmeldungen.</returns>
+        public Task<ICollection<KatWarnMessage>> GetKatWarnMessages();
+    }
+}
